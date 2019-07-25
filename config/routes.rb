ab0e1b95d3e50db5b49624e354b84a33f79128ec
Simona Cappeddu <simona.cappeddu@gmail.com>
2019-07-25 21:37:32 +0100

    restaurant controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+  end
 end
